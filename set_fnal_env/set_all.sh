9014c6f9c5d4fb9da2fe8aca15000523858da965
lperes-ufrj <leoperes.ufrj@gmail.com>
2026-08-07 11:48:15 -0500

    [EDIT] new varibles to set environment

--- set_fnal_env/set_all.sh
+++ set_fnal_env/set_all.sh
@@ -1,12 +1,13 @@
 source set_protodune_vd.sh
 source dune_token.sh
 source dune_data.sh
+echo $USER
 export INPUT_TAR_DIR_LOCAL=/cvmfs/fifeuser2.opensciencegrid.org/sw/dune/6b669427c337376a060d5e4a47a17f3efd81a009
 export FCL_FILE="$INPUT_TAR_DIR_LOCAL/get_pds.fcl"
 export FNALURL="https://fndcadoor.fnal.gov:2880/"
-export USERF="dune/scratch/users/lperes/sag_analysis"
-export SAG_SC="/pnfs/dune/scratch/users/lperes/sag_analysis"
-export SAG_PERS="/pnfs/dune/persistent/users/lperes/sag_analysis"
-export SAG_DIR="/exp/dune/app/users/lperes/sag_analysis"
+export USERF="dune/scratch/users/$USER/sag_analysis"
+export SAG_SC="/pnfs/dune/scratch/users/$USER/sag_analysis"
+export SAG_PERS="/pnfs/dune/persistent/users/$USER/sag_analysis"
+export SAG_DIR="/exp/dune/app/users/$USER/sag_analysis"
 #source /nashome/d/dbrailsf/setupVNCNew.sh
 #export DISPLAY=:56159
