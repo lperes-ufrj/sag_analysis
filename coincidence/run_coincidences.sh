242e57dae08a4c5d6223b735420c8eab6b53f69a
lperes-ufrj <leoperes.ufrj@gmail.com>
2026-08-14 11:59:36 -0500

    [ADD] new run with 9k adc amplitude cut and peak finder

--- coincidence/run_coincidences.sh
+++ coincidence/run_coincidences.sh
@@ -21,7 +21,7 @@ for input_file in input_lists/input_run*.txt; do
         --config waveform_intervals.ini \
         --channels-coincident-left 2030 2031 2040 2041 \
         --channels-coincident-right 2050 2051 2060 2061 \
-        --channels-to-save 2070 2071 2080 2081 \
+        --channels-to-save 2080 2081 \
         --window-ticks 10 \
         --min-amplitude-adc 0
 
