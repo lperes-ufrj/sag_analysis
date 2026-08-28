1ab64de0481fe3749519e533f2749b76d58f5fe2
lperes-ufrj <leoperes.ufrj@gmail.com>
2026-08-28 14:21:44 -0500

    [ADD] standard coincidence scan with reorganized code

--- coincidence/run_full_pipeline_mean_wfs.sh
+++ coincidence/run_full_pipeline_mean_wfs.sh
@@ -1,5 +1,5 @@
 analysis_timestamp=$(date +%Y%m%d_%H%M%S)
 
-.run_coincidences.sh "$analysis_timestamp"
+./run_coincidences.sh "$analysis_timestamp"
 
-.run_plot_wfs_all.sh "$analysis_timestamp"
+./run_plot_wfs_all.sh "$analysis_timestamp"
